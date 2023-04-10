825e7ab204c3d3edddb0893546faac118a35990c
piotrcurious <78758456+piotrcurious@users.noreply.github.com>
2023-04-11 00:50:23 +0200

    Create kirhoff_good.ino

---
+++ kirhoff_good.ino
@@ -0,0 +1,73 @@
+
+// Define constants and variables
+const int outputPin = 9; // digital output pin connected to RC circuit
+const int inputPin = A0; // analog input pin connected to capacitor
+const float capacitance = 10e-6; // capacitance of capacitor in farads
+const float maxCurrent = 0.02; // maximum current in amperes
+const int maxPulse = 1000; // maximum pulse length in microseconds
+const int interval = 60000; // interval between resistance measurements in milliseconds
+float resistance; // resistance of resistor in ohms
+unsigned long previousMillis = 0; // previous time of measurement in milliseconds
+
+void setup() {
+  // Initialize serial communication
+  Serial.begin(9600);
+  // Set output pin to low
+  pinMode(outputPin, OUTPUT);
+  digitalWrite(outputPin, LOW);
+}
+
+void loop() {
+  // Get current time in milliseconds
+  unsigned long currentMillis = millis();
+  // Check if interval has passed since last measurement
+  if (currentMillis - previousMillis >= interval) {
+    // Update previous time
+    previousMillis = currentMillis;
+    // Call function to measure resistance
+    resistance = measureResistance();
+    // Print resistance value to serial monitor
+    Serial.print("Resistance: ");
+    Serial.print(resistance);
+    Serial.println(" ohms");
+  }
+}
+
+// Function to measure resistance of RC circuit
+float measureResistance() {
+  float r; // resistance value to return
+  int maxVoltage = 0; // maximum voltage reading from analog input pin
+  int pulseLength = 0; // pulse length that gives maximum voltage reading
+  int voltage; // voltage reading from analog input pin
+  
+  // Loop through different pulse lengths from 1 to maxPulse microseconds
+  for (int i = 1; i <= maxPulse; i++) {
+    // Set output pin to high for i microseconds
+    digitalWrite(outputPin, HIGH);
+    delayMicroseconds(i);
+    // Set output pin to low and read voltage from input pin
+    digitalWrite(outputPin, LOW);
+    voltage = analogRead(inputPin);
+    // Check if voltage is higher than previous maximum
+    if (voltage > maxVoltage) {
+      // Update maximum voltage and pulse length values
+      maxVoltage = voltage;
+      pulseLength = i;
+    }
+  }
+  
+  // Convert maximum voltage reading to volts (assuming 5V reference)
+  float vMax = maxVoltage * (5.0 / 1023.0);
+  
+  // Calculate resistance using Kirchhoff's voltage law and Ohm's law
+  // Vout = Vin * (1 - exp(-t / (RC)))
+  // Vout / Vin = (1 - exp(-t / (RC)))
+  // ln(1 - Vout / Vin) = -t / (RC)
+  // RC = -t / ln(1 - Vout / Vin)
+  // R = -t / (C * ln(1 - Vout / Vin))
+  
+  r = -pulseLength * 1e-6 / (capacitance * log(1 - vMax / 5.0));
+  
+  // Return resistance value
+  return r;
+}
